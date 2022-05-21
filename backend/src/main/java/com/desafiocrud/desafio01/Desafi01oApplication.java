3b656bd7f2b145ac895cf3c3d3a81d53f565d09f
Diego Rodrigues <diegokod@gmail.com>
2022-05-21 11:34:43 -0300

    Project Created

---
+++ backend/src/main/java/com/desafiocrud/desafio01/Desafi01oApplication.java
@@ -0,0 +1,13 @@
+package com.desafiocrud.desafio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafi01oApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafi01oApplication.class, args);
+	}
+
+}
